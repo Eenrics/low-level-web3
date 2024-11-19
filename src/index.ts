a7e885126223b6ab5ef76883d700ce6dbd8a93bf
Abenezer Eshetie <ebenezeresh525@gmail.com>
2024-11-20 02:38:39 +0300

    contract events

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { ethers } from "ethers";
+import Counter from "../artifacts/contracts/Counter.sol/Counter.json";
 
 function getEth() {
     const eth = window.ethereum;
@@ -27,28 +28,38 @@ async function run() {
     const counter = new ethers.Contract(
         // "0x5fbdb2315678afecb367f032d93f642f64180aa3",
         process.env.CONTRACT_ADDRESS!,
-        [
-            // "function hello() public pure returns (string memory)"
-            "function count() public",
-            "function getCounter() public view returns (uint)",
-        ],
+        // [
+        //     // "function hello() public pure returns (string memory)"
+        //     "function count() public",
+        //     "function getCounter() public view returns (uint)",
+        // ],
+
+        Counter.abi,
         new ethers.providers.Web3Provider(getEth()).getSigner(), // (new ethers.providers.Web3Provider(getEth())) a provider tells where the network is (basic communication)
         // (getSigner()) a signer accesses the wallet information and allows requests to be signed by the user.
     )
 
     const el = document.createElement("div");
-    async function setCounter() {
-        el.innerHTML = await counter.getCounter();
+    async function setCounter(count?) {
+        el.innerHTML = count || await counter.getCounter();
     }
     setCounter();
 
     const button = document.createElement("button");
     button.innerHTML = "Increment";
     button.onclick = async () => {
-        const tx = await counter.count();
-        await tx.wait()
-        setCounter();
+        // const tx = await counter.count();
+        // await tx.wait()
+        // setCounter();
+
+        // Better to do it with contract events
+        await counter.count();
     }
+
+    counter.on(counter.filters.CounterInc(), function(count) {
+        setCounter(count)
+    })
+
     // document.body.innerHTML = await counter.hello();
     document.body.appendChild(el);
     document.body.appendChild(button);
